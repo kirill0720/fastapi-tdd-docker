b632bbce9d8f1140b4982cc003f6c522acb99ab3
Kirill Netreba <knete-87@mail.ru>
2023-07-30 22:51:34 +0300

    Can post summaries, write it to DB and has test for post

--- project/app/main.py
+++ project/app/main.py
@@ -1,27 +1,30 @@
 import logging
-from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    log.info(" Starting up...")
-    init_db(app)
-    yield
-    log.info("Shutting down...")
-
-
 def create_application() -> FastAPI:
-    application = FastAPI(lifespan=lifespan)
+    application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
 
 app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
