a6f2264c8c4c836b29b9c83ccd2fd137a9e31124
Kirill Netreba <knete-87@mail.ru>
2023-07-30 23:45:46 +0300

    Add GET single summary Route and test for it

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,5 +1,5 @@
 from app.models.pydantic import SummaryPayloadSchema
-from app.models.tortoise import TextSummary
+from app.models.tortoise import TextSummary, SummarySchema
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
@@ -9,3 +9,10 @@ async def post(payload: SummaryPayloadSchema) -> int:
     )
     await summary.save()
     return summary.id
+
+
+async def get(id: int) -> SummarySchema | None:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary
+    return None
